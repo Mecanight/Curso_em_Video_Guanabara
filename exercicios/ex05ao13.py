07a5dc0d56c6cd2843298fddcac3462c6fc5fa46
Mecanight <cassianoprediger@gmail.com>
2022-05-21 20:43:29 -0300

    Create ex05ao13.py

---
+++ exercicios/ex05ao13.py
@@ -0,0 +1,48 @@
+
+#exercicio 05
+# numero = int(input('Digite um numero'))
+# sucessor = numero + 1
+# antecessor = numero - 1
+# print(f'O antecessor é {antecessor} e o sucessor é {sucessor}')
+
+#exercicio 06
+# numero = int(input('Digite um número'))
+# print(f'O dobro é {numero*2}, o triplo é {numero*3} e a raiz quadrada é {numero**(1/2)}')
+
+#exercicio 07
+# nota1 = float(input('Diga a primeira nota'))
+# nota2 = float(input('Diga a segunda nota'))
+# media = (nota1 + nota2) / 2
+# print(f'A média é {media}')
+
+#exercicio 08
+# metro = float(input('Digite uma distancia em metros'))
+# print(f'São {metro*100} centímetros')
+# print(f'São {metro*1000} milímetros')
+
+#exercicio 09
+# numero = int(input('Digite um numero para a tabuada'))
+# for i in range(0,11):
+#     print(f'{numero} * {i} = {numero*i}')
+
+#exercicio 10
+# dinheiro = float(input('Informe quanto dinheiro você tem na carteira'))
+# dolar = dinheiro / 3.27
+# print(f'Isso equivale a {dolar} dolares')
+
+#exercicio 11
+# altura = float(input('Informe a altura da parede'))
+# largura = float(input('Informe a largura da parede'))
+# area = altura * largura
+# tinta = area / 2
+# print(f'A parede tem {area:.2f} m² e irá precisar de {tinta:.3f} litros de tinta para pintá-la')
+
+#exercicio 12
+# preco = float(input('Diga o preço do produto'))
+# precoajuste = preco * 0.95
+# print(f'Com 5% de desconto fica {precoajuste:.2f}')
+
+#exercicio 13
+# salario = float(input('Informe seu salario'))
+# salarioajuste = salario * 1.15
+# print(f'Com aumento de 15% fica {salarioajuste:.3f}')
