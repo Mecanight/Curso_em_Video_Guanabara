706344379f2c090f0365f5f3427082f991a4a611
Mecanight <cassianoprediger@gmail.com>
2022-05-21 20:43:18 -0300

    Create ex003.py

---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite um numero'))
+numero2 = float(input('Digite outro numero'))
+soma = numero1 + numero2
+print('A soma dos 02 é:', soma)
+print('A soma dos 02 é {}'.format(soma))
+print('A soma entre', numero1, 'e', numero2, 'é', soma)
+print('A soma entre {} e {} é {}' .format(numero1, numero2, soma))
