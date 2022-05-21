21a04278a6005dbbe6481344afabc84f1c3c0d2b
Mecanight <cassianoprediger@gmail.com>
2022-05-21 20:43:02 -0300

    Create notas_codigos.py

---
+++ notas_codigos.py
@@ -0,0 +1,67 @@
+# print('Ola, Mundo!')
+# print(7+4)
+# print('7'+'4')
+# 7+4
+# '7'+'4'
+# print('Ola', 5)
+# nome = 'Guanabara'
+# idade = 25
+# peso = 75.8
+# print(nome, idade, peso)
+# nome = input('Qual é o seu nome?')
+# idade = input('Quantos anos você tem?')
+# peso = input('Qual é o seu peso')
+# print(nome, idade, peso)
+
+# nome = input('Qual é o seu nome?')
+# print('Seja bem Vindo', nome)
+
+# dia = input('Que dia você nasceu?')
+# mes = input('Que mês você nasceu?')
+# ano = input('Que ano você nasceu?')
+# print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+
+# numero_1 = int(input('Informe um número'))
+# numero_2 = int(input('Informe outro número'))
+# soma = numero_1 + numero_2
+# print('A soma dos dois números é:', soma)
+# print(f'A soma entre {numero_1} e {numero_2} é {soma}')
+#
+# n = input('Digite algo')
+# print(type(n))
+# print(n)
+# print(n.isnumeric())
+# print(n.isalpha())
+# print(n.upper())
+
+
+#AULA 07
+
+# print('19+2 =',19+2)
+# print('19-2 =',19-2)
+# print('19*2 =',19*2)
+# print('19/2 =',19/2)
+# print('19**2 =',19**2)
+# print('19//2 =',19//2)
+# print('19%2 =',19%2)
+#
+# nome = input('Qual é seu nome')
+# print('Prazer em te conhecer {nome:20}!')
+# print('Prazer em te conhecer {nome:>20}!')
+# print('Prazer em te conhecer {nome:<20}!')
+# print('Prazer em te conhecer {nome:^20}!')
+
+# n1 = int(input('Um valor: '))
+# n2 = int(input('Outro valor'))
+# soma = n1+n2
+# subt = n1-n2
+# mult = n1*n2
+# divi = n1/n2
+# divint = n1//n2
+# poten = n1**n2
+#
+#
+# print(f'A soma é {soma}, a subtração é {subt}, o produto é {mult}, a divisão é {divi:.2f},', end = " ")
+# print(f'a divisão inteira é {divint} e a potencia é {poten}')
+
+
