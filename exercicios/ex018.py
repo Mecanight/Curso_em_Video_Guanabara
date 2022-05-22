3c61d3237b02169797c72dc3bd2aec731ee39df1
Mecanight <cassianoprediger@gmail.com>
2022-05-22 20:11:34 -0300

    Create ex018.py

---
+++ exercicios/ex018.py
@@ -0,0 +1,6 @@
+import math
+angulo = int(input('Diga o angulo'))
+seno = math.sin(angulo)
+cosseno = math.cos(angulo)
+print(f'O seno do angulo {angulo} é {seno}')
+print(f'O cosseno do angulo {angulo} é {cosseno}')
