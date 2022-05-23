7d1ab82c1e77f24741ac736036dd7fe1df60e007
Mecanight <cassianoprediger@gmail.com>
2022-05-22 22:08:27 -0300

    Update ex017.py

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -1,7 +1,8 @@
-import math
+from math import hypot
 catopo = float(input('Diga o cateto oposto'))
 catadj = float(input('Diga o cateto adjacente'))
 
-hipo = math.hypot(catopo, catadj)
-print(f'A hipotenusa é {hipo}')
+# hipo = ((catopo**2) + (catadj**2))** (1/2)
 
+hipo = hypot(catopo, catadj)
+print(f'A hipotenusa é {hipo:.2f}')
