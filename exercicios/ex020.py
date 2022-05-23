9acd80db7e619b0b3dd8a33e4216574c60491623
Mecanight <cassianoprediger@gmail.com>
2022-05-22 22:08:37 -0300

    Update ex020.py

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,21 +1,22 @@
-import random
-import array as ar
-a1 = 'cleber'
-a2 = 'joao'
-a3 = 'jose'
-a4 = 'maria'
+from random import shuffle
+a1 = input("Diga o nome do aluno ")
+a2 = input("Diga o nome do aluno ")
+a3 = input("Diga o nome do aluno ")
+a4 = input("Diga o nome do aluno ")
 
 lista = [a1, a2, a3, a4]
+shuffle(lista)
+print(f'A orde de apresentação é \n {lista}')
 
-sorteio1 = random.choice(lista)
-lista.remove(sorteio1)
-sorteio2 = random.choice(lista)
-lista.remove(sorteio2)
-sorteio3 = random.choice(lista)
-lista.remove(sorteio3)
-sorteio4 = random.choice(lista)
-print('A orde da apresentações é a seguinte')
-print(sorteio1)
-print(sorteio2)
-print(sorteio3)
-print(sorteio4)
+# sorteio1 = random.choice(lista)
+# lista.remove(sorteio1)
+# sorteio2 = random.choice(lista)
+# lista.remove(sorteio2)
+# sorteio3 = random.choice(lista)
+# lista.remove(sorteio3)
+# sorteio4 = random.choice(lista)
+# print('A orde da apresentações é a seguinte')
+# print(sorteio1)
+# print(sorteio2)
+# print(sorteio3)
+# print(sorteio4)
