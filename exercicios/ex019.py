694e0a1df411c944499dee4db0955805e6b8ce79
Mecanight <cassianoprediger@gmail.com>
2022-05-22 22:08:35 -0300

    Update ex019.py

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,16 +1,20 @@
-import random
-a1 = input("Diga o nome do aluno")
-a2 = input("Diga o nome do aluno")
-a3 = input("Diga o nome do aluno")
-a4 = input("Diga o nome do aluno")
+from random import choice
+a1 = input("Diga o nome do aluno ")
+a2 = input("Diga o nome do aluno ")
+a3 = input("Diga o nome do aluno ")
+a4 = input("Diga o nome do aluno ")
 
-apaga = random.randint(1, 4)
-print(apaga)
-if (apaga == 1):
-    print(f'{a1} apaga o quadro')
-if (apaga == 2):
-    print(f'{a2} apaga o quadro')
-if (apaga == 3):
-    print(f'{a3} apaga o quadro')
-if (apaga == 4):
-    print(f'{a4} apaga o quadro')
+lista = [a1, a2, a3, a4]
+
+escolha = choice(lista)
+print(f'{escolha} irá apagar o quadro')
+
+# escolha = choice(lista)
+# if (escolha == a1):
+#     print(f'{a1} apaga o quadro')
+# if (escolha == a2):
+#     print(f'{a2} apaga o quadro')
+# if (escolha == a3):
+#     print(f'{a3} apaga o quadro')
+# if (escolha == a4):
+#     print(f'{a4} apaga o quadro')
