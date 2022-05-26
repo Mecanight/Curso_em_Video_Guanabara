fb0872fdd4c11a6f48ef0c6a78b4a4df300e9996
Mecanight <cassianoprediger@gmail.com>
2022-05-26 00:45:11 -0300

    Update notas_codigos.py

--- notas_codigos.py
+++ notas_codigos.py
@@ -84,3 +84,47 @@
 # import random
 # num = random.randint(1, 10)
 # print(num)
+
+# AULA 09
+
+# print("""Formações com conteúdo do mercado de trabalho:
+# sequências de cursos e conteúdo para você se capacitar
+# em tecnologia e negócios digitais.
+# São 1338 cursos e novos lançamentos toda semana, além
+# de atualizações e melhorias constantes.
+# Garantimos conhecimento com profundidade e diversidade,
+# para se tornar um profissional em T!
+# Faça parte de uma comunidade de apaixonados por tudo que
+# é digital. Mergulhe na comunidade Alura.""")
+
+frase = 'Curso em Video Python'
+print(frase[9])
+print(frase[9:13])
+print(frase[9:21])
+print(frase[9:21:2])
+print(frase[:5])
+print(frase[15:])
+print(frase[9::3])
+print(len(frase))
+print(frase.count('o'))
+print(frase.count('o', 0, 13))
+print(frase.find('deo'))
+print(frase.find('Android'))
+print('Curso' in frase)
+alterar = frase.replace('Python', 'Android')
+print(alterar)
+print(frase.upper())
+print(frase.lower())
+print(frase.capitalize())
+print(frase.title())
+frase2 = '   Aprenda Python  '
+print(frase2)
+print(frase2.strip())
+print(frase2.rstrip())
+print(frase2.lstrip())
+frase_split = frase.split()
+print(frase_split)
+print(frase_split[0])
+print(frase_split[0][3])
+frase_join = '-'.join(frase_split)
+print(frase_join)
